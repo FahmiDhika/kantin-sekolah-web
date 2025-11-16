fa0af24d5ea48b836e843b715dcb97f2937a32e4
Fahmi Dhika <fahmidhika810@gmail.com>
2025-11-16 17:35:30 +0700

    tambahin middleware ketika user blm login ga bisa akses halaman laen

---
+++ middleware.ts
@@ -0,0 +1,47 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export const middleware = async (req: NextRequest) => {
+  const token = req.cookies.get("token")?.value;
+  const role = req.cookies.get("role")?.value;
+
+  if (req.nextUrl.pathname === "/") {
+    const redirectAdmin = req.nextUrl.clone();
+    redirectAdmin.pathname = "/login";
+    return NextResponse.redirect(redirectAdmin);
+  }
+
+  if (req.nextUrl.pathname.startsWith("/stan")) {
+    if (!token || !role) {
+      const redirectAdmin = req.nextUrl.clone();
+      redirectAdmin.pathname = "/login";
+      return NextResponse.redirect(redirectAdmin);
+    }
+
+    if (role !== "ADMIN_STAN") {
+      const redirectAdmin = req.nextUrl.clone();
+      redirectAdmin.pathname = "/login";
+      return NextResponse.redirect(redirectAdmin);
+    }
+    return NextResponse.next();
+  }
+
+  if (req.nextUrl.pathname.startsWith("/siswa")) {
+    if (!token || !role) {
+      const redirectAdmin = req.nextUrl.clone();
+      redirectAdmin.pathname = "/login";
+      return NextResponse.redirect(redirectAdmin);
+    }
+
+    if (role !== "SISWA") {
+      const redirectAdmin = req.nextUrl.clone();
+      redirectAdmin.pathname = "/login";
+      return NextResponse.redirect(redirectAdmin);
+    }
+    return NextResponse.next();
+  }
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/stan/:path", "/siswa/:path", "/"],
+};
